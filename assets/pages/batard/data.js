2a42822404ffa00cbed207babaaceeaf46f8470e
Farid <farid.hammouten@gmail.com>
2017-12-10 09:39:44 +0100

    merge master

---
+++ assets/pages/batard/data.js
@@ -0,0 +1,412 @@
+klynt.data = {
+    "aliases": {
+        "idToAlias": {
+            "6C4758FF-14C1-C41A-11D6-3DCE19EADFB5": "Brass'art",
+            "8F9DC2C3-C90B-10B0-A048-3DD13D40EF5B": "expli_brass'art"
+        },
+        "aliasToId": {
+            "Brass'art": "6C4758FF-14C1-C41A-11D6-3DCE19EADFB5",
+            "expli_brass'art": "8F9DC2C3-C90B-10B0-A048-3DD13D40EF5B"
+        }
+    },
+    "wording": {
+        "total_sequences": "séquences au total",
+        "keywords": "mots-clés",
+        "your_are_here": "Vous êtes ici",
+        "total_duration": "(durée totale : {0})",
+        "program_percentage_viewed": "du programme vu"
+    },
+    "general": {
+        "overlayCloseButtonTop": 16,
+        "primaryColor": 3355443,
+        "title": "Taxi Babel",
+        "level": 3,
+        "projectHost": "",
+        "secondaryColor": 16777215,
+        "editorVersionNumber": "3.5.005",
+        "projectId": "3A4A3448-0B56-7524-EE7B-3D6C938896A7",
+        "analyticsKey": "",
+        "tertiaryColor": 15286861,
+        "editorVersionLabel": "Klynt 3.5.005",
+        "creationDate": 1512859014025,
+        "mapsKey": "",
+        "mainFontName": "Open Sans",
+        "width": 970,
+        "overlayCloseButtonRight": 16,
+        "mainFontSize": 14,
+        "height": 545,
+        "modificationDate": 1512893567697,
+        "keywords": null,
+        "description": "",
+        "ratio": "16/9",
+        "username": "paulinezec",
+        "author": "paulinezec"
+    },
+    "share": {
+        "enableTumblr": true,
+        "link": null,
+        "verticalWording": "Vertical",
+        "displayMiniPlayerWording": true,
+        "originalSizeWording": "Original",
+        "playerWording": "Player",
+        "removeViaKlynt": true,
+        "horizontalWording": "Horizontal",
+        "customSizeWording": "Personnalisé",
+        "miniPlayerWording": "Mini player",
+        "shareSequenceWording": "Partager la séquence",
+        "linkWording": "Lien",
+        "shortMessage": "",
+        "socialNetworksWording": "Réseaux sociaux",
+        "thumbnail": null,
+        "title": "",
+        "enableGooglePlus": true,
+        "message": "",
+        "enableLinkedIn": true,
+        "embedWording": "Intégrer"
+    },
+    "watermark": {
+        "action": {
+            "type": "null",
+            "target": "null"
+        },
+        "displayOnStartSequence": true,
+        "image": "Player/css/editor/img/watermark.png",
+        "enabled": false,
+        "position": "null"
+    },
+    "advanced": {
+        "enableScrollNavigation": true,
+        "hasRemoteVideos": false,
+        "remoteVideosAPILocalBitrate": "best",
+        "enableSwipeNavigation": true,
+        "remoteVideosAPIURL": "http://api.klynt.net/video/klynt-video-api.php",
+        "enableFullWindowMode": true,
+        "enableKeyboardNavigation": true
+    },
+    "footer": {
+        "height": 26,
+        "items": [
+            {
+                "action": {
+                    "type": "openWidget",
+                    "target": "E8DFEA4A-E2A1-379A-94D9-3D6C944B3A0D"
+                },
+                "label": "Index"
+            },
+            {
+                "action": {
+                    "type": "openWidget",
+                    "target": "E7F72DA2-DB47-61E8-8C71-3D6C9449076B"
+                },
+                "label": "Crédits"
+            }
+        ],
+        "buttons": [
+            {
+                "icon": "share",
+                "type": "share",
+                "label": "Partage"
+            },
+            {
+                "icon": "sound",
+                "type": "sound",
+                "label": "Son"
+            },
+            {
+                "icon": "fullscreen",
+                "type": "fullscreen",
+                "label": "Plein Ecran"
+            },
+            {
+                "icon": "klynt",
+                "type": "klynt",
+                "label": "Made with"
+            }
+        ],
+        "logo": {
+            "url": "http://www.klynt.net",
+            "tooltip": "null",
+            "image": null
+        }
+    },
+    "menu": {
+        "searchWidget": "51894E67-FAB4-4F27-E911-3D6C944A457E",
+        "widgets": [
+            {
+                "id": "E8DFEA4A-E2A1-379A-94D9-3D6C944B3A0D",
+                "params": {
+                    "sequences": [
+                        "6C4758FF-14C1-C41A-11D6-3DCE19EADFB5",
+                        "8F9DC2C3-C90B-10B0-A048-3DD13D40EF5B"
+                    ],
+                    "displayDuration": true,
+                    "displayDescriptions": true,
+                    "layout": "grid"
+                },
+                "type": "index",
+                "label": "Index"
+            },
+            {
+                "id": "E7F72DA2-DB47-61E8-8C71-3D6C9449076B",
+                "params": {
+                    "subtitle": "",
+                    "aboutColumnText": "",
+                    "title": "Taxi Babel",
+                    "creditsColumnTitle": "Crédits",
+                    "creditsColumnText": "",
+                    "aboutColumnTitle": "A Propos"
+                },
+                "type": "credits",
+                "label": "Crédits"
+            },
+            {
+                "placeHolderWording": "Tapez ici...",
+                "id": "51894E67-FAB4-4F27-E911-3D6C944A457E",
+                "resultWording": "Résultat pour : ",
+                "resultsWording": "Résultats pour : ",
+                "type": "search",
+                "enabled": true,
+                "label": "Recherche"
+            },
+            {
+                "smallSequenceSize": 25,
+                "displayWatermark": false,
+                "type": "mindmap",
+                "label": "Mindmap",
+                "largeSequenceSize": 100,
+                "displayLinksArrow": true,
+                "id": "B148F2CC-D5D5-A77B-07CB-3D6C944AB71B",
+                "mediumSequenceSize": 60,
+                "displayDescriptions": false
+            }
+        ],
+        "displayBackgroundImage": false,
+        "backgroundImage": null,
+        "offset_sequence": 12
+    },
+    "sequences": {
+        "6C4758FF-14C1-C41A-11D6-3DCE19EADFB5": {
+            "id": "6C4758FF-14C1-C41A-11D6-3DCE19EADFB5",
+            "title": "Brass'art",
+            "backgroundColor": "rgba(0,0,0,1)",
+            "images": [
+                {
+                    "id": "ABFAFF70-92BC-3AEC-8BE1-3DCE902AA86C",
+                    "src": "Medias/Photos/IMG_8352.jpg",
+                    "horizontalAlign": "center",
+                    "transitionIn": {
+                        "duration": "1",
+                        "type": "fade"
+                    },
+                    "databegin": "00:00.00",
+                    "name": "IMG_8352.jpg",
+                    "dataend": "00:31.00",
+                    "mediaWidth": 969,
+                    "verticalAlign": "middle",
+                    "duration": "00:31.00",
+                    "zIndex": 1,
+                    "mediaHeight": 646,
+                    "fitToWindow": true,
+                    "scaleMode": "zoom"
+                },
+                {
+                    "scales": true,
+                    "id": "ECC29179-26AF-EC24-9428-3DD080F3F4BD",
+                    "src": "Medias/Photos/triangle.png",
+                    "horizontalAlign": "center",
+                    "transitionIn": {
+                        "duration": "1",
+                        "type": "fade"
+                    },
+                    "left": "156.23",
+                    "width": "49.80",
+                    "name": "triangle.png",
+                    "dataend": "00:31.00",
+                    "mediaWidth": 500,
+                    "databegin": "00:00.00",
+                    "duration": "00:31.00",
+                    "zIndex": 3,
+                    "rollOver": [
+                        {
+                            "duration": 0.5,
+                            "easing": "linear",
+                            "delay": 0,
+                            "reversible": true,
+                            "target": "0EF0CBDC-379E-48BE-5821-3DD214ED4411",
+                            "name": "opacity",
+                            "type": "style",
+                            "value": "1"
+                        }
+                    ],
+                    "top": "91.47",
+                    "height": "49.80",
+                    "verticalAlign": "middle",
+                    "scaleMode": "zoom",
+                    "mediaHeight": 500
+                }
+            ],
+            "y": 590,
+            "x": 342,
+            "thumbnail": "Medias/Thumbnails/IMG_8352.jpg.jpg",
+            "hideInMindmap": false,
+            "size": "large",
+            "duration": 30,
+            "shapes": [
+                {
+                    "scales": true,
+                    "id": "8B8CB4AB-315E-9765-D4A6-3DD476C3CDB3",
+                    "left": "423.30",
+                    "width": "46.70",
+                    "transitionIn": {
+                        "duration": "1",
+                        "type": "fade"
+                    },
+                    "label": "Pulsar 1",
+                    "dataend": "00:31.00",
+                    "databegin": "00:00.00",
+                    "duration": "00:31.00",
+                    "zIndex": 5,
+                    "top": "290.15",
+                    "height": "46.70",
+                    "link": {
+                        "controlDX": 0,
+                        "id": "DE46F9EC-A63F-2490-3309-3DD4BE69F73F",
+                        "hideInMindmap": false,
+                        "linkType": "linkToSequence",
+                        "automaticTransition": false,
+                        "hideArrowInMindmap": false,
+                        "transition": "fade",
+                        "tooltip": "Entrer",
+                        "controlDY": 0,
+                        "color": "rgba(0,0,0,1)",
+                        "targetSequence": "8F9DC2C3-C90B-10B0-A048-3DD13D40EF5B"
+                    },
+                    "type": "pulsar",
+                    "style": {
+                        "backgroundColor": "rgba(255,255,255,1)"
+                    }
+                }
+            ],
+            "hideThumbnailInMindmap": false,
+            "texts": [
+                {
+                    "scales": true,
+                    "id": "5749988B-A92E-1A14-6340-3DCF88C014E5",
+                    "text": "<p><strong>Le Brass'Art</strong></p>",
+                    "left": "87.06",
+                    "width": "228.24",
+                    "name": "Texte 1",
+                    "dataend": "00:31.00",
+                    "databegin": "00:00.00",
+                    "duration": "00:31.00",
+                    "zIndex": 2,
+                    "top": "57.24",
+                    "height": "71.55",
+                    "type": "txt-Title-3",
+                    "transitionIn": {
+                        "duration": "1",
+                        "type": "fade"
+                    }
+                },
+                {
+                    "scales": true,
+                    "id": "0EF0CBDC-379E-48BE-5821-3DD214ED4411",
+                    "text": "<p><span style=\"background-color: #999999;\">Bla bla bla bla bla bla bla bla explication Brass'art</span></p>\n<p>&nbsp;</p>",
+                    "left": "47.64",
+                    "width": "267.13",
+                    "name": "Texte 2",
+                    "dataend": "00:31.00",
+                    "transitionIn": {
+                        "duration": "1",
+                        "type": "fade"
+                    },
+                    "databegin": "00:00.00",
+                    "duration": "00:31.00",
+                    "zIndex": 4,
+                    "height": "71.55",
+                    "type": "txt-Paragraph",
+                    "centerY": "-88.85",
+                    "style": {
+                        "opacity": 0
+                    }
+                }
+            ],
+            "hideTitleInMindmap": false,
+            "color": "rgba(0,0,0,1)"
+        },
+        "8F9DC2C3-C90B-10B0-A048-3DD13D40EF5B": {
+            "id": "8F9DC2C3-C90B-10B0-A048-3DD13D40EF5B",
+            "title": "expli brass'art",
+            "backgroundColor": "rgba(0,0,0,1)",
+            "images": [
+                {
+                    "id": "54BC39C2-5486-FB36-7B0B-3DD9AA64BF99",
+                    "src": "Medias/Photos/IMG_8329.jpg",
+                    "horizontalAlign": "center",
+                    "transitionIn": {
+                        "duration": "1",
+                        "type": "fade"
+                    },
+                    "databegin": "00:00.00",
+                    "name": "IMG_8329.jpg",
+                    "dataend": "00:31.00",
+                    "mediaWidth": 969,
+                    "verticalAlign": "middle",
+                    "duration": "00:31.00",
+                    "zIndex": 1,
+                    "mediaHeight": 646,
+                    "fitToWindow": true,
+                    "scaleMode": "zoom"
+                }
+            ],
+            "y": 809,
+            "x": 341,
+            "thumbnail": "Medias/Thumbnails/IMG_8329.jpg.jpg",
+            "hideInMindmap": false,
+            "size": "medium",
+            "duration": 30,
+            "shapes": [
+                {
+                    "scales": true,
+                    "id": "4FDFB839-3783-8138-F9EB-3DD9CF34B7B6",
+                    "type": "pulsar",
+                    "right": "48.23",
+                    "transitionIn": {
+                        "duration": "1",
+                        "type": "fade"
+                    },
+                    "bottom": "160.42",
+                    "label": "Pulsar 1",
+                    "dataend": "00:31.00",
+                    "width": "45.71",
+                    "duration": "00:31.00",
+                    "zIndex": 2,
+                    "height": "45.71",
+                    "databegin": "00:00.00",
+                    "style": {
+                        "backgroundColor": "rgba(255,255,255,1)"
+                    }
+                }
+            ],
+            "hideThumbnailInMindmap": false,
+            "hideTitleInMindmap": false,
+            "color": "rgba(0,0,0,1)"
+        }
+    },
+    "miniPlayer": {
+        "thanksForWatchingWording": "Merci de votre visite !",
+        "title": "Taxi Babel",
+        "launchAppWording": "Et lancer le projet",
+        "downloadAppWording": "Télécharger l'app",
+        "description": "",
+        "thumbnail": "Medias/Photos/IMG_8352.jpg",
+        "resumePlaybackWording": "Relancer la lecture ?",
+        "redirectToMobileApp": "auto",
+        "noWording": "Non",
+        "url": "",
+        "yesWording": "Oui",
+        "fullscreenInfoWording": "Ce programme va se lancer en plein écran",
+        "analyticsKey": ""
+    },
+    "mainSequence": "6C4758FF-14C1-C41A-11D6-3DCE19EADFB5"
+}
---
+++ assets/pages/batard/miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "thanksForWatchingWording": "Merci de votre visite !",
+    "title": "Taxi Babel",
+    "launchAppWording": "Et lancer le projet",
+    "downloadAppWording": "Télécharger l'app",
+    "description": "",
+    "thumbnail": "Medias/Photos/IMG_8352.jpg",
+    "resumePlaybackWording": "Relancer la lecture ?",
+    "redirectToMobileApp": "auto",
+    "noWording": "Non",
+    "url": "",
+    "yesWording": "Oui",
+    "fullscreenInfoWording": "Ce programme va se lancer en plein écran",
+    "analyticsKey": ""
+}
